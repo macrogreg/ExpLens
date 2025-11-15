73856beac56d08d60ffd5a06cbc66919c96b5252
macrogreg <macrogreg@users.noreply.github.com>
2025-11-14 20:06:39 -0800

    Tob bar layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,11 @@
 <template>
     <q-page class="row justify-center">
         <div class="col-8 q-pa-md" style="max-width: 800px">
-            <div class="q-mb-md">
-                <h4 class="q-my-sm">LunchMoney Excel Add-In</h4>
+            <div class="row items-center no-wrap title-bar">
+                <h1 class="col">LunchMoney Sync</h1>
+                <div class="col-auto">
+                    <img src="/icons/favicon-32x32.png" alt="Logo" style="height: 32px; width: 32px" />
+                </div>
             </div>
 
             <q-tabs v-model="tab" align="left" class="q-mb-md">
@@ -17,6 +20,22 @@
         </div>
     </q-page>
 </template>
+
+<style scoped lang="css">
+.title-bar {
+    background-color: rgb(31, 105, 194);
+    padding: 2px 15px 2px 15px;
+    color: white;
+}
+h1 {
+    font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+    font-weight: 400;
+    font-size: 32px;
+    line-height: normal;
+    margin: 0;
+    padding: 5px 0 5px 0;
+}
+</style>
 
 <script setup lang="ts">
 import { ref } from "vue";
