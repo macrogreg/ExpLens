a5b95ab6b7314978c1cd5b4d6cb49f4bc6a675ea
macrogreg <macrogreg@users.noreply.github.com>
2025-11-14 19:37:28 -0800

    Simplify Layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,39 +1,27 @@
 <template>
-    <q-page class="row items-center justify-evenly">
-        <example-component title="Example component" active :todos="todos" :meta="meta"></example-component>
+    <q-page class="row justify-center">
+        <div class="col-8 q-pa-md" style="max-width: 800px">
+            <div class="q-mb-md">
+                <h4 class="q-my-sm">LunchMoney Excel Add-In</h4>
+            </div>
+
+            <q-tabs v-model="tab" align="left" class="q-mb-md">
+                <q-tab name="syncData" label="Sync Data" />
+                <q-tab name="advanced" label="Advanced" />
+            </q-tabs>
+
+            <q-separator class="q-mb-md" />
+
+            <sync-data-tab v-if="tab === 'syncData'" />
+            <advanced-tab v-if="tab === 'advanced'" />
+        </div>
     </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import type { Todo, Meta } from "components/models";
-import ExampleComponent from "components/ExampleComponent.vue";
+import SyncDataTab from "components/SyncDataTab.vue";
+import AdvancedTab from "components/AdvancedTab.vue";
 
-const todos = ref<Todo[]>([
-    {
-        id: 1,
-        content: "ct1",
-    },
-    {
-        id: 2,
-        content: "ct2",
-    },
-    {
-        id: 3,
-        content: "ct3",
-    },
-    {
-        id: 4,
-        content: "ct4",
-    },
-    {
-        id: 5,
-        content: "ct5",
-    },
-]);
-
-const meta = ref<Meta>({
-    totalCount: 1200,
-});
+const tab = ref("syncData");
 </script>
-
